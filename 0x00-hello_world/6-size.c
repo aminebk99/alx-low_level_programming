cca6b614361899417b36ab6c44fe7c2aae96b150
bazaou <mohamedaminebk99@gmail.com>
2023-07-13 19:17:41 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- 
-main - A program that print a line with printf sizeof variable
-Return: 0 (Success)
+*main - A program that print a line with printf sizeof variable
+*Return: 0 (Success)
 */
 int main(void)
 {
-    char a;
-    int b;
-    long int c;
-    long long int d;
-        float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
