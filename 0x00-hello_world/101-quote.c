7e3de5d45e67e19ccd58666370c3d321a1eb09ac
bazaou <mohamedaminebk99@gmail.com>
2023-07-13 19:47:07 -0700

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+*main - program that prints exactly and that piece of art is useful
+*Return: 1
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+return (0);
+}
