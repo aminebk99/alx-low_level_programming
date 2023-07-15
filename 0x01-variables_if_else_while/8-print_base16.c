c824dd08c21320e99a37e67c71bd5a199cad5c0d
bazaou <mohamedaminebk99@gmail.com>
2023-07-14 18:54:20 -0700

    condition

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - number
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
