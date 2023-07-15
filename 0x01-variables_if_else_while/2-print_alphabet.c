a9a1a1e5ff9d8c49d23bdd1c0ebab9caa9e7fffd
bazaou <mohamedaminebk99@gmail.com>
2023-07-14 17:53:03 -0700

    condition

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints tha alphabet in lowercase
+* Return: 0
+*/
+int main(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
