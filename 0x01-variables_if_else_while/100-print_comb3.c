fc5d4f8c7bd2088f4169c69c4fe160412477d851
bazaou <mohamedaminebk99@gmail.com>
2023-07-14 19:02:22 -0700

    condition

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+* main - Entry Point
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int num1, num2;
+for (num1 = 48; num1 <= 57; num1++)
+{
+for (num2 = 48; num2 <= 57; num2++)
+if (num1 < num2)
+{
+putchar(num1);
+putchar(num2);
+if (num2 != 57 || num1 != 56)
+{
+putchar(44);
+putchar(32);                }
+}
+}
+putchar(10);
+return (0);
+}
