7af0862fda18fc02eadad735082f59f97fca9382
bazaou <mohamedaminebk99@gmail.com>
2023-07-14 17:58:13 -0700

    condition

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char i = 'a';
+char x = 'A';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+while (x <= 'Z')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
