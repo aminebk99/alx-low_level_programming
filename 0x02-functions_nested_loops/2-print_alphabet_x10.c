cf8e5b9a49143003b4165fa219f5384bbe1b5ad5
bazaou <mohamedaminebk99@gmail.com>
2023-07-18 04:58:32 -0700

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - make alphabet 10 times
+ * Return void
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+i++;
+}
+}
