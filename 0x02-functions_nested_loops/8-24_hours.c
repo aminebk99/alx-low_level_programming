ee0e667a348b5c9ab143322268cfd00e05e2ff4b
bazaou <mohamedaminebk99@gmail.com>
2023-07-18 10:51:49 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the last digit of a number
+ *
+ * Return: (0)
+ */
+void jack_bauer(void)
+{
+int hour = 0;
+int min = 0;
+while (hour < 24)
+{
+while (min < 60)
+{
+_putchar (hour / 10 + 48);
+_putchar (hour % 10 + 48);
+_putchar (':');
+_putchar (min / 10 + 48);
+_putchar (min % 10 + 48);
+min++;
+_putchar ('\n');
+}
+min = 0;
+hour++;
+}
+}
