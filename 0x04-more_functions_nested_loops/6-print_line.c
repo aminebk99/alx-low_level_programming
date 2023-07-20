6d40d2d3e8c5e035c8d9cf0cc9bf0ecd1bfdb980
bazaou <mohamedaminebk99@gmail.com>
2023-07-20 11:35:18 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a diagonal line on the terminal.
+ * @n : parameter
+ */
+void print_line(int n)
+{
+int l;
+
+if (n > 0)
+{
+for (l = 0; l < n; l++)
+_putchar('_');
+}
+_putchar('\n');
+}
