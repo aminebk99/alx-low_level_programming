3072d04625c02caa21f04863f1f6a69281487e7e
bazaou <mohamedaminebk99@gmail.com>
2023-07-26 16:09:01 -0700

    fix 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,13 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rev_array - reverse array
+ * rev_string - reverse array
  * @n: integer params
  * Return: 0
  */
 
-void rev_array(char *n)
+void rev_string(char *n)
 {
 int i = 0;
 int j = 0;
@@ -38,8 +37,8 @@ temp = *(n + j);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int carry = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, total = 0;
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
 
 while (*(n1 + i) != '\0')
 i++;
@@ -49,7 +48,7 @@ i--;
 j--;
 if (j >= size_r || i >= size_r)
 return (0);
-while (j >= 0 || i >= 0 || carry == 1)
+while (j >= 0 || i >= 0 || overflow == 1)
 {
 if (i < 0)
 val1 = 0;
@@ -59,21 +58,21 @@ if (j < 0)
 val2 = 0;
 else
 val2 = *(n2 + j) - '0';
-total = val1 + val2 + carry;
-if (total >= 10)
-carry = 1;
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
 else
-carry = 0;
+overflow = 0;
 if (digits >= (size_r - 1))
 return (0);
-r[digits] = (total % 10) + '0';
+*(r + digits) = (temp_tot % 10) + '0';
 digits++;
 j--;
 i--;
 }
 if (digits == size_r)
 return (0);
-r[digits] = '\0';
-rev_array(r);
+*(r + digits) = '\0';
+rev_string(r);
 return (r);
 }
