e59d59d34aaf5e55b850f3c5e7691bbfd4a76a72
bazaou <mohamedaminebk99@gmail.com>
2023-08-01 21:34:05 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s : string
+ * Return: string
+*/
+void _puts_recursion(char *s)
+{
+if (*s  == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _second_sqrt_recursion - helpes function to calculate the square root
+ * @n: the number to calculate
+ * @i: help to calculate
+ *
+ * Return: final result
+ */
+int _second_sqrt_recursion(int n, int i)
+{
+if (i * i > n)
+return (-1);
+else if (i * i == n)
+return (i);
+else
+return (_second_sqrt_recursion(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return (_second_sqrt_recursion(n, 0));
+}
