3db11252e85ea475f7d83a90382c94608ce31990
amine <mohamedaminebk99@gmailcom>
2023-11-23 04:23:06 -0800

    add print binary and get bit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_binary - function that converts a binary
+ *						number to an unsigned int
+ * @n: number to print in binary
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+print_binary(n >> 1);
+
+if (n & 1)
+_putchar('1');
+else
+_putchar('0');
+}
