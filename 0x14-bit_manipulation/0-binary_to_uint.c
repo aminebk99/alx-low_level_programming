a832be07858c3a4ea3c75ba75f0086e0af2302a2
amine <mohamedaminebk99@gmailcom>
2023-11-23 04:51:47 -0800

    update binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,20 @@
 #include "main.h"
-
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string representing a binary number.
- *
- * Return: The unsigned int value of the binary number, or 0 if b is NULL or
- *         contains a character that is not '0' or '1'.
+ * binary_to_uint - converter dyal munber int
+ * @b: string fin kayn num
+ * Return: lhadak  numb
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int total, power;
-int len;
-
-if (b == NULL)
+int i;
+unsigned int af_val = 0;
+if (!b)
 return (0);
-
-for (len = 0; b[len]; len++)
+for (i = 0; b[i]; i++)
 {
-if (b[len] != '0' && b[len] != '1')
+if (b[i] < '0' || b[i] > '1')
 return (0);
+af_val = 2 * af_val + (b[i] - '0');
 }
-
-for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-{
-if (b[len] == '1')
-total += power;
-}
-
-return (total);
+return (af_val);
 }
