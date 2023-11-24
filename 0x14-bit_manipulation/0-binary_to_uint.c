5a6244db9e2b901ecb3aa63ed5c28b2cbdec27c0
amine <mohamedaminebk99@gmailcom>
2023-11-24 14:41:46 -0800

    update 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,30 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * binary_to_uint - converter dyal munber int
- * @b: string fin kayn num
- * Return: lhadak  numb
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int af_val = 0;
-if (!b)
+unsigned int total, power;
+int len;
+
+if (b == NULL)
 return (0);
-for (i = 0; b[i]; i++)
+
+for (len = 0; b[len]; len++)
 {
-if (b[i] < '0' || b[i] > '1')
+if (b[len] != '0' && b[len] != '1')
 return (0);
-af_val = 2 * af_val + (b[i] - '0');
 }
-return (af_val);
+
+for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+{
+if (b[len] == '1')
+total += power;
+}
+
+return (total);
 }
