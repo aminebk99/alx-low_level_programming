01fece3b4217c212633d5bc160a4a5fd2c3266e4
amine <mohamedaminebk99@gmailcom>
2024-01-11 16:01:45 -0800

    commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,33 +10,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current = *h;
-	unsigned int count = 0;
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (current != NULL && count < idx - 1)
+	for (; idx != 1; idx--)
 	{
-		current = current->next;
-		count++;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 
-	if (current == NULL)
-		return (NULL);
-
-	if (current->next == NULL)
+	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->prev = current;
-	new_node->next = current->next;
-	current->next->prev = new_node;
-	current->next = new_node;
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-	return (new_node);
+	return (new);
 }
